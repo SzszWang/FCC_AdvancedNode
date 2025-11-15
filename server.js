c843468b96694d44d9880cb24e7755254739a947
王斯郁 <szyu@wangsiyudeMacBook-Air.local>
2025-11-15 20:04:07 +0800

    update

--- server.js
+++ server.js
@@ -8,19 +8,13 @@ const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.set('view engine', 'pug');
-app.set('views', './views/pug');
+app.set('views', express.static(process.cwd() + '/views/pug'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('index', {
-    title: 'FCC Advanced Node and Express',      // 給 title
-    message: 'This is the pug template working', // 給 message
-    showLogin: false,
-    showRegistration: false,
-    showSocialAuth: false
-  });
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
