035c4aa739b76cc59239b66aa4bed4e136c01de4
王斯郁 <szyu@wangsiyudeMacBook-Air.local>
2025-11-15 20:38:17 +0800

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const cors = require('cors'); 
+const path = require('path');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -10,15 +11,21 @@ const app = express();
 fccTesting(app); //For FCC testing purposes
 app.use(cors());
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
-app.set('views', './views/pug');
+app.set('views', path.join(__dirname, 'views/pug'));
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'FCC Advanced Node and Express',
+    message: 'Pug is working!',
+    showLogin: false,
+    showRegistration: false,
+    showSocialAuth: false
+  });
 });
 
 const PORT = process.env.PORT || 3000;
